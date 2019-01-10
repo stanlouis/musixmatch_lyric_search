38219e90f69e51536b61d2f0421b9a9276141007
Stanley Louis <stanlouis@msn.com>
2019-01-10 07:57:55 -0500

    Add request to get album release date

--- src/components/tracks/Lyrics.js
+++ src/components/tracks/Lyrics.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-
 import { Link } from "react-router-dom";
 import Moment from "react-moment";
 
@@ -9,7 +8,8 @@ import Spinner from "../layout/Spinner";
 class Lyrics extends Component {
   state = {
     track: {},
-    lyrics: {}
+    lyrics: {},
+    album: ""
   };
 
   componentDidMount() {
@@ -30,60 +30,70 @@ class Lyrics extends Component {
         });
       })
       .then(res => {
+        const { album_id } = res.data.message.body.track;
         this.setState({ track: res.data.message.body.track });
+
+        return musixmatch.get("/album.get", {
+          params: {
+            album_id: album_id
+          }
+        });
+      })
+      .then(res => {
+        this.setState({
+          album: res.data.message.body.album
+        });
       })
       .catch(err => console.log(err));
   }
 
   render() {
-    const { track, lyrics } = this.state;
-
-    if (
-      track === undefined ||
+    const { track, lyrics, album } = this.state;
+    return track === undefined ||
       lyrics === undefined ||
       Object.keys(track).length === 0 ||
-      Object.keys(lyrics).length === 0
-    ) {
-      return <Spinner />;
-    } else {
-      return (
-        <React.Fragment>
-          <Link to="/" className="btn btn-dark btn-sm mb-4">
-            Go Back
-          </Link>
-          <div className="card">
-            <h5 className="card-header">
-              {track.track_name} by{" "}
-              <span className="text-secondary">{track.artist_name}</span>
-            </h5>
-            <div className="card-body">
-              <p className="card-text">{lyrics.lyrics_body}</p>
-            </div>
+      Object.keys(lyrics).length === 0 ? (
+      <Spinner />
+    ) : (
+      <React.Fragment>
+        <Link to="/" className="btn btn-dark btn-sm mb-4">
+          Go Back
+        </Link>
+        <div className="card">
+          <h5 className="card-header">
+            {track.track_name} by{" "}
+            <span className="text-secondary">{track.artist_name}</span>
+          </h5>
+          <div className="card-body">
+            <p className="card-text">{lyrics.lyrics_body}</p>
           </div>
+        </div>
 
-          <ul className="list-group mt-3">
-            <li className="list-group-item">
-              <strong>Album ID</strong>: {track.album_id}
-            </li>
-            <li className="list-group-item">
-              <strong>Song Genre</strong>:{" "}
-              {track.primary_genres.music_genre_list.length === 0
-                ? "NO GENRE AVAILABLE"
-                : track.primary_genres.music_genre_list[0].music_genre
-                    .music_genre_name}
-            </li>
-            <li className="list-group-item">
-              <strong>Explicit Words</strong>:{" "}
-              {track.explicit === 0 ? "No" : "Yes"}
-            </li>
-            <li className="list-group-item">
-              <strong>Release Date</strong>:{" "}
-              <Moment format="MM/DD/YYYY">{track.first_release_date}</Moment>
-            </li>
-          </ul>
-        </React.Fragment>
-      );
-    }
+        <ul className="list-group mt-3">
+          <li className="list-group-item">
+            <strong>Album Name</strong>: {track.album_name}
+          </li>
+          <li className="list-group-item">
+            <strong>Song Genre</strong>:{" "}
+            {track.primary_genres.music_genre_list.length === 0
+              ? "NO GENRE AVAILABLE"
+              : track.primary_genres.music_genre_list[0].music_genre
+                  .music_genre_name}
+          </li>
+          <li className="list-group-item">
+            <strong>Explicit Words</strong>:{" "}
+            {track.explicit === 0 ? "No" : "Yes"}
+          </li>
+          <li className="list-group-item">
+            <strong>Album Rating</strong>: {album.album_rating}
+          </li>
+          <li className="list-group-item">
+            <strong>Release Date</strong>:{" "}
+            <Moment format="MM/DD/YYYY">{album.album_release_date}</Moment>
+          </li>
+        </ul>
+      </React.Fragment>
+    );
   }
 }
 
